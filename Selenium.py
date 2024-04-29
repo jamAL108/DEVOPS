6f326116bc214960eb20300ab82e13ee076baeb4
Jamal Mydeen <jamalmydeen112@gmail.com>
2024-04-30 00:42:11 +0530

    Update Selenium.py

--- Selenium.py
+++ Selenium.py
@@ -12,7 +12,7 @@ driver.find_element(By.NAME, "q").send_keys("bing")
 driver.find_element(By.NAME, "q").send_keys(Keys.ENTER)
 driver.find_element(By.PARTIAL_LINK_TEXT,"Microsoft Bing").click()
 time.sleep(5)
-driver.find_element(By.NAME,"searchbox").send_keys(("Python"))
 print("Program finished")
 time.sleep(100)
 driver.close()
+
